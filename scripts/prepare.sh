d563a4cf37353d518c13f77251af60d3530d81e0
maiSamirMohammad <88589466+maiSamirMohammad@users.noreply.github.com>
2025-08-20 03:35:12 +0300

    regenerate cicd-project-release-key.jks.base64

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,4 @@
-echo $KEYSTORE_BASE64 | base64 --decode > cicd-project-release-key.jks
+echo $KEYSTORE_BASE64 | base64 -d > app/cicd-project-release-key.jks
 ./gradlew clean
 bundle install
 bundle exec fastlane install_plugins
